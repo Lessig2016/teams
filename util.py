7f021a41845750b8f2ebfc54dc16c1c11b5586de
Gabriel Grant <g@briel.ca>
2015-08-25 06:30:27 -0400

    fix leaderboard position numbers

--- util.py
+++ util.py
@@ -9,16 +9,19 @@ import config_NOCOMMIT
 def leaderboardGetter(offset, limit, orderBy):
   leaderboard = config_NOCOMMIT.pledge_service.getLeaderboard(
       offset=offset, limit=limit, orderBy=orderBy)
+  def team_iter():
+    for team_data in leaderboard:
+      if team_data["total_cents"] == 0:
+          continue
+      if team_data["team"].startswith('{{team.key()}}'):
+          # sentinel value == no team
+          continue
+      team = Team.get(db.Key(team_data["team"]))
+      if team is None:
+        continue
+      yield team_data, team
   teams = []
-  for idx, team_data in enumerate(leaderboard):
-    if team_data["total_cents"] == 0:
-        continue
-    if team_data["team"].startswith('{{team.key()}}'):
-        # sentinel value == no team
-        continue
-    team = Team.get(db.Key(team_data["team"]))
-    if team is None:
-      continue
+  for idx, (team_data, team) in enumerate(team_iter()):
     teams.append({
         "amount": int(team_data["total_cents"] / 100),
         "num_pledges":int(team_data["num_pledges"]),
